14eec9f9309a79e209c2eee5e57d603bda36ca62
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-01-16 10:45:39 -0300

    feat: adding functions conditions and while

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,1 +1,27 @@
-alert('Bem-vndo ao jogo do número secreto!')
+alert('Bem-vndo ao jogo do número secreto!');
+
+let secretNumber = 29;
+console.log(secretNumber);
+
+let userResponse;
+let trys = 1;
+
+while (userResponse != secretNumber) {
+
+    userResponse = prompt('Escolha um número entre 1 e 30.')
+
+    if (userResponse == secretNumber) {
+        break;
+    } else {
+        if (userResponse > secretNumber) {
+            alert(`O número secreto é menor que ${userResponse}`);
+        } else {
+            alert(`O número secreto é maior que ${userResponse}`);
+        }
+        // trys = trys + 1
+        trys++;
+    }
+}
+
+let wordTry = trys > 1 ? 'tentativas' : 'tentativa'
+alert(`Você é o bixão mesmo em, o número secreto é ${secretNumber}, acertou com ${trys} ${wordTry}`);
